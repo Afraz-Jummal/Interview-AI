edc0c426cadace7976159e52372c454be2924e50
Afraz-Jummal <afrazjummal12@gmail.com>
2026-06-30 19:02:34 +0530

    fix UI

--- Frontend/src/features/auth/pages/Register.jsx
+++ Frontend/src/features/auth/pages/Register.jsx
@@ -17,8 +17,12 @@ const Register = () => {
         navigate("/")
     }
 
-    if(loading){
-        return (<main><h1>Loading.......</h1></main>)
+    if (loading ) {
+        return (
+            <main className='loading-screen'>
+                <h1>Loading your interview plan...</h1>
+            </main>
+        )
     }
 
     return (
